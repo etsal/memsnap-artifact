30851d36a132cfc105304296e96c2d49d665fdb7
Emil Tsalapatis <emil@etsalapatis.com>
2024-03-09 01:32:27 -0500

    removed extraneous build.sh script

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd aurora-memsnap; make -j9; make install; cd -
-cd aurora-original; make -j9; cd -
-cd microbenchmarks; make -j9; cd -
-cd sqlite; ./setup.sh; cd -
-cd rocksdb; ./setup.sh; cd -
