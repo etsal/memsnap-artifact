633865f8759537bc272cefb713bd6f1009e1c79c
Emil Tsalapatis <emil@etsalapatis.com>
2024-03-11 20:48:33 -0400

    streamline setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-# Initialize all the submodules
-git submodule update --init aurora-original
-git submodule update --init aurora-memsnap
-git submodule update --init FlameGraph
-git submodule update --init sqlite/auroravfs
-git submodule update --init sqlite/sqlite
-git submodule update --init sqlite/db_bench
-git submodule update --init sqlite/tatp
-
 # Install packages included with this artifact.
 cp -r packages /packages
 
@@ -21,26 +12,33 @@ env PACKAGESITE=file:/packages pkg bootstrap
 pkg add /packages/bash-5.2.15.pkg
 chsh -s "/usr/local/bin/bash"
 
-
 for p in `ls /packages/*.pkg`; 
-	do pkg add $p; 
+	do /bin/sh -c "pkg add $p";
 done
 
 # Remove incompatible DTrace definitions
 rm /usr/lib/dtrace/psinfo.d
 rm /usr/lib/dtrace/ip.d
 
+# Initialize all the submodules
+git submodule update --init aurora-original
+git submodule update --init aurora-memsnap
+git submodule update --init FlameGraph
+git submodule update --init sqlite/auroravfs
+git submodule update --init sqlite/sqlite
+git submodule update --init sqlite/db_bench
+git submodule update --init sqlite/tatp
+
 # Set up bootloader options
-mv loader.conf /boot/loader.conf
+cat loader.conf >> /boot/loader.conf
 
 # Get a base root, required by the benchmarks
 wget https://rcs.uwaterloo.ca/~etsal/base.txz
 mkdir -p /usr/freebsd-dist
 mv base.txz /usr/freebsd-dist
 
-git clone https://github.com/rcslab/aurora-12.3.git
-rmdir /usr/src; mv aurora-12.3 /usr/src
-
+rmdir /usr/src;
+git clone https://github.com/rcslab/aurora-12.3.git /usr/src
 
 mkdir -p /usr/lib/debug/boot/modules/
 mkdir -p /usr/aurora/tests/posix/
